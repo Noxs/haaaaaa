ee66641b8974fba44f3708f431c2c25a1aea3d5d
Félix Darraud <felixdarraud@mbp-de-felix.home>
2017-12-22 09:33:51 +0100

    Rework filters

---
+++ filters/filterTest.js
@@ -0,0 +1,7 @@
+function filterTest(){
+    return new Promise(function(resolve, reject) {
+        resolve();
+    });
+}
+
+module.exports = filterTest;
--- test/filtersTest.js
+++ test/filtersTest.js
@@ -5,8 +5,81 @@ const should = chai.should();
 const filters = require('../lib/filters.js');
 
 describe('Filters', function () {
-    it('Filters _init() method : Success', function () {
+    it('Filters build : Success', function () {
+        assert.isFunction(filters.translate);
         assert.isFunction(filters.dayTest);
         assert.isFunction(filters.halfTest);
+    });
+
+    it('Filters add() method : Success', function () {
+        const filter = {
+            process : require('../filters/filterTest.js'),
+            name : 'filterTest'
+        };
+        filters.add(filter);
+        assert.isFunction(filters.filterTest);
+    });
+
+    it('Filters add() method : First parameter is not an object', function () {
+        const testFunc = function () {
+            filters.add('This is not an object');
+        };
+        expect(testFunc).to.throw('First parameter of filters add() method must be an object.');
+    });
+
+    it('Filters add() method : First parameter process attribute is not defined.', function () {
+        const filter = {
+            name : 'filterTest'
+        };
+        const testFunc = function () {
+            filters.add(filter);
+        };
+        expect(testFunc).to.throw('First parameter of filters add() method must have a process attribute.');
+    });
+
+    it('Filters add() method : First parameter process value is not a function.', function () {
+        const filter = {
+            process : 'this is not a function',
+            name : 'filterTest'
+        };
+        const testFunc = function () {
+            filters.add(filter);
+        };
+        expect(testFunc).to.throw('First parameter\'s process attribute of filters add() method must be a function.');
+    });
+
+    it('Filters add() method : First parameter name attribute is not defined.', function () {
+        const filter = {
+            process : require('../filters/filterTest.js'),
+        };
+        const testFunc = function () {
+            filters.add(filter);
+        };
+        expect(testFunc).to.throw('First parameter of filters add() method must have a name attribute.');
+    });
+
+    it('Filters add() method : First parameter name attribute is not a string.', function () {
+        const filter = {
+            process : require('../filters/filterTest.js'),
+            name : {data : 'This is not a string'}
+        };
+        const testFunc = function () {
+            filters.add(filter);
+        };
+        expect(testFunc).to.throw('First parameter\'s name attribute of filters add() method must be a string.');
+    });
+
+    it('Filters applyFilter() method : The filter is not defined in the filters instance', function (done) {
+        const tag = [ '{{ test | notExisting }}', 'test', 'notExisting', undefined ];
+        const variable = 'test';
+        const context = {data : 'Value'};
+        filters.applyFilter(tag, variable, context).then( (result) => {
+            assert.isUndefined(result);
+            done();
+        }, (error) => {
+            assert.equal(error.message, 'Filter ' + tag[2] + ' is not defined, it might not be added.');
+            done();
+        });
+
     });
 });
